fe89d9e5dd76adae287930f3ba79181388c85916
kboysm <lishal12@naver.com>
2021-09-01 17:49:48 +0900

    macos m1 chip error sucess

--- lib/screens/edit.dart
+++ lib/screens/edit.dart
@@ -1,10 +1,63 @@
+import 'package:first_flutter_project/database/db.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../database/memo.dart';
 
 class EditPage extends StatelessWidget {
-  const EditPage({Key? key}) : super(key: key);
+  String title = '';
+  String text = '';
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue);
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
+            IconButton(onPressed: saveDB, icon: const Icon(Icons.save)),
+          ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(children: [
+            TextField(
+                onChanged: (String title) {
+                  this.title = title;
+                },
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                // obscureText: true, 패스워드 처럼 **로 나오게하는 옵션
+                decoration: InputDecoration(
+                    // border: OutlineInputBorder(),
+                    hintText: '제목')),
+            Padding(padding: EdgeInsets.all(10)),
+            TextField(
+                onChanged: (String text) {
+                  this.text = text;
+                },
+                // obscureText: true, 패스워드 처럼 **로 나오게하는 옵션
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                    // border: OutlineInputBorder(),
+                    hintText: '내용')),
+          ]),
+        ));
+  }
+
+  Future<void> saveDB() async {
+    DBHelper sd = DBHelper();
+
+    var fido = Memo(
+      id: 3,
+      title: this.title,
+      text: this.text,
+      createTime: DateTime.now().toString(),
+      editTime: DateTime.now().toString(),
+    );
+
+    await sd.insertMemo(fido);
+
+    print(await sd.memos());
   }
 }
