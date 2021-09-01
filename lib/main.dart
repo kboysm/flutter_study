362bc7cc57ac5edaec2f7c4852176b9ab437999b
kboysm <lishal12@naver.com>
2021-09-01 13:47:31 +0900

    tutorial 마무리

--- lib/main.dart
+++ lib/main.dart
@@ -51,13 +51,29 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
     var buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Column(children: [Icon(Icons.call), Text('Call')]),
-        Column(children: [Icon(Icons.near_me), Text('Call')]),
-        Column(children: [Icon(Icons.share), Text('Call')])
+        Column(children: [
+          Icon(Icons.call, size: 45, color: Colors.lightBlue),
+          Text('Call', style: TextStyle(color: Colors.lightBlue))
+        ]),
+        Padding(padding: EdgeInsets.all(40.0)),
+        Column(children: [
+          Icon(Icons.near_me, size: 45, color: Colors.lightBlue),
+          Text('Route', style: TextStyle(color: Colors.lightBlue))
+        ]),
+        Padding(padding: EdgeInsets.all(40.0)),
+        Column(children: [
+          Icon(Icons.share, size: 45, color: Colors.lightBlue),
+          Text('Share', style: TextStyle(color: Colors.lightBlue))
+        ])
       ],
     );
-    var textSection = Text('asdasd');
+    var textSection = Container(
+        padding: EdgeInsets.all(30.0),
+        child: Text(
+            'To show right-aligned metadata (assuming left-to-right reading order; left-aligned for right-to-left reading order), consider using a Row with CrossAxisAlignment.baseline alignment whose first item is Expanded and whose second child is the metadata text, instead of using the trailing property.',
+            style: TextStyle(fontSize: 20)));
     return Scaffold(
       body: Column(children: [
         Image.network(
@@ -65,8 +81,11 @@ class _MyHomePageState extends State<MyHomePage> {
             height: 240,
             width: 600,
             fit: BoxFit.cover),
+        Padding(padding: EdgeInsets.all(15.0)),
         titleSection,
+        Padding(padding: EdgeInsets.all(15.0)),
         buttonSection,
+        // Padding(padding: EdgeInsets.all(15.0)),
         textSection
       ]),
     );
